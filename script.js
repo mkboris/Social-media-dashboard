fb1f49ee862a2ac7ff451e0809340f731f9d14e1
makoge boris <makogeboris@gmail.com>
2024-05-19 21:56:33 +0100

    modified prefered color scheme

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const toogleName = document.querySelector(".toogle-name");
+
+function getCurrentTheme() {
+  let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+
+  localStorage.getItem("dashboard")
+    ? (theme = localStorage.getItem("dashboard"))
+    : null;
+
+  return theme;
+}
+
+function loadTheme(theme) {
+  const root = document.querySelector(":root");
+  root.setAttribute("color-scheme", `${theme}`);
+}
+
+darkModeToggle.addEventListener("click", () => {
+  let theme = getCurrentTheme();
+  if (theme === "dark") {
+    theme = "light";
+    toogleName.textContent = "Dark Mode";
+  } else {
+    theme = "dark";
+    toogleName.textContent = "Light Mode";
+  }
+  localStorage.setItem("dashboard", `${theme}`);
+  loadTheme(theme);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadTheme(getCurrentTheme());
+});
